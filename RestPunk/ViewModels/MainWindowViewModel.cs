5cfbd6f641870b082c11a68b41df2c15bb7a2fd7
Eric Fortner <Eric.Fortner@digitalcloak.net>
2025-10-22 23:02:44 -0400

    Initial commit, setting up UI for Avalonia using community toolkit.

---
+++ RestPunk/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RestPunk.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ RestPunk/ViewModels/SavedQueryViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RestPunk.ViewModels
+{
+    public class SavedQueryViewModel : ViewModelBase
+    {
+        public ObservableCollection<QueryFolder> QueryFolders { get; set; }
+
+        public SavedQueryViewModel()
+        {
+            QueryFolders = new ObservableCollection<QueryFolder>();
+            QueryFolders.Add(new QueryFolder
+            {
+                Name = "Test Folder",
+                SavedQueries = new ObservableCollection<SavedQuery>() 
+                { 
+                    new SavedQuery
+                    {
+                        Name = "Test query",
+                        URI = "https://www.restpunk.com",
+                        HttpVerb = "GET"
+                    }
+                }
+            });
+           
+
+        }
+    }
+
+    public class QueryFolder
+    {
+        public string Name { get; set; }
+        public ObservableCollection<SavedQuery> SavedQueries { get; set; }
+    }
+
+    public class SavedQuery
+    {
+        public string Name { get; set; }
+        public string URI { get; set; }
+        public string HttpVerb { get; set; }
+    }
+}
